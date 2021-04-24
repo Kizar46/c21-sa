20cb667e080e3a2fa4ddef14fdc92c35ac511a2d
Kizar46 <82276145+Kizar46@users.noreply.github.com>
2021-04-24 20:43:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var platform1,platform2;
+var countDistanceX=0
+var gap=60
+function preload()
+{}
+
+function setup() {
+  createCanvas(1360,625);
+  for(var i=0; i<6;i++){
+  platform1=new Platform(countDistanceX);
+  countDistanceX=countDistanceX+platform1.rw+gap
+  }
+  gap=random(0,0,0,0,60)
+}
+
+function draw() {
+  background('skyblue'); 
+ drawSprites();
+}
+
